18866ac17332cc1c3773433b3577b6ae88fd6e59
Richard Steinbrueck <richard.steinbrueck@userlike.com>
2023-02-28 11:41:09 +0100

    feat(automation): Update screenshots by script

---
+++ bin/update_screenshots.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+if [[ -n $TMUX ]]; then
+  echo "💣 Script dont work in a tmux, please close at tmux sessions and try again!"
+  exit 255
+fi
+
+# screenshot-1
+tmux new-session -d -s demo -d
+tmux split-window -t "demo:1" -h
+tmux split-window -t "demo:1" -v
+
+tmux send-keys -t "demo:1.1" 'cd Projects/3rd-party/prometheus/ && ls -la' Enter
+tmux send-keys -t "demo:1.2" 'clear && neofetch' Enter
+tmux send-keys -t "demo:1.3" 'top' Enter
+
+sh -c "sleep 4; tmux kill-session" &
+screencapture -T2 .dotfileassets/screenshot-1.png &
+
+tmux attach -tdemo
+
+# screenshot-2
+tmux new-session -d -s demo -d
+tmux split-window -t "demo:1" -h
+
+tmux send-keys -t "demo:1.1" 'clear' Enter
+tmux send-keys -t "demo:1.1" 'cd Projects/3rd-party/prometheus/' Enter
+tmux send-keys -t "demo:1.1" 'vim' Enter
+tmux send-keys -t "demo:1.2" 'cd Projects/3rd-party/prometheus/' Enter
+tmux send-keys -t "demo:1.2" 'tig' Enter
+tmux send-keys -t "demo:1.2" Enter
+
+sh -c "sleep 4; tmux kill-session" &
+screencapture -T2 .dotfileassets/screenshot-2.png &
+
+tmux attach -tdemo
