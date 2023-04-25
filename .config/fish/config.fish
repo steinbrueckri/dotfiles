05a0fa07852b78a650c6391e0fb635fd306afd3f
Richard Steinbrueck <richard.steinbrueck@userlike.com>
2023-04-25 16:08:18 +0200

    💾 cleanup ssh setup

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -193,9 +193,100 @@ export ANSIBLE_FORCE_COLOR=1
 # export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES before your Ansible run should clear it up. The code that's causing issues is well below Ansible in the stack.
 export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
 
+
+################################################################################
+# ssh-agent magic
+################################################################################
+function sshagent_findsockets
+	find /tmp -uid (id -u) -type s -name agent.\* 2>/dev/null
+end
+
+function sshagent_testsocket
+    if [ ! -x (command which ssh-add) ] ;
+        echo "ssh-add is not available; agent testing aborted"
+        return 1
+    end
+
+    if [ X"$argv[1]" != X ] ;
+    	set -xg SSH_AUTH_SOCK $argv[1]
+    end
+
+    if [ X"$SSH_AUTH_SOCK" = X ]
+    	return 2
+    end
+
+    if [ -S $SSH_AUTH_SOCK ] ;
+        ssh-add -l > /dev/null
+        if [ $status = 2 ] ;
+            echo "Socket $SSH_AUTH_SOCK is dead!  Deleting!"
+            rm -f $SSH_AUTH_SOCK
+            return 4
+        else ;
+            echo "Found ssh-agent $SSH_AUTH_SOCK"
+            return 0
+        end
+    else ;
+        echo "$SSH_AUTH_SOCK is not a socket!"
+        return 3
+    end
+end
+
+
+function ssh_agent_init
+    # ssh agent sockets can be attached to a ssh daemon process or an
+    # ssh-agent process.
+
+    set -l AGENTFOUND 0
+
+    # Attempt to find and use the ssh-agent in the current environment
+    if sshagent_testsocket ;
+        set AGENTFOUND 1
+    end
+
+    # If there is no agent in the environment, search /tmp for
+    # possible agents to reuse before starting a fresh ssh-agent
+    # process.
+    if [ $AGENTFOUND = 0 ];
+        for agentsocket in (sshagent_findsockets)
+            if [ $AGENTFOUND != 0 ] ;
+	            break
+            end
+            if sshagent_testsocket $agentsocket ;
+	       set AGENTFOUND 1
+	    end
+
+        end
+    end
+
+    # If at this point we still haven't located an agent, it's time to
+    # start a new one
+    if [ $AGENTFOUND = 0 ] ;
+	echo need to start a new agent
+	eval (ssh-agent -c)
+    end
+
+    # Finally, show what keys are currently in the agent
+    # ssh-add -l
+end
+
 #######################################################################
 #                              functions                              #
 #######################################################################
+
+set ssh_agent_sock ~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock
+if test -e $ssh_agent_sock
+  set -xg SSH_AUTH_SOCK $ssh_agent_sock
+else
+  echo "1password ssh agent.sock not found, start ssh-agent"
+  echo "Please load your ssh key with the command loadsshkey!"
+  ssh_agent_init
+end
+
+# Start ssh-agent, load key from 1password and added to the ssh-agent
+function loadsshkey
+  eval $(op signin --account pixel-combo.1password.com)
+  op item get jr4k7znbfh5g4zadnd3zgfnjli --fields Private-Key | sed 's/\"//g' | ssh-add -
+end
 
 function gcp-project
     set FILE "$HOME/.gcp-project-list.json"
