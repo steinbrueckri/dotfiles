438f770e191f33643db70e4a1dbeba0570ab824f
Richard Steinbrueck <richard.steinbrueck@userlike.com>
2023-07-12 11:16:05 +0200

    💾 save!

--- .config/nvim/lua/plugins/zmk.lua
+++ .config/nvim/lua/plugins/zmk.lua
@@ -1,16 +1,13 @@
 local conf = {
-  name = "LAYOUT_pretty",
-  variant = 'zmk',
-  layout = {
-    "x x x x x x x _ _ _ _ _ _ x x x x x x x",
-    "x x x x x x x _ _ _ _ _ _ x x x x x x x",
-    "x x x x x x x _ _ _ _ _ _ x x x x x x x",
-    "x x x x x x _ _ _ _ _ _ _ _ x x x x x x",
-    "x x x x x _ _ _ _ _ _ _ _ _ _ x x x x x",
-    "_ _ _ _ _ _ x x _ _ _ _ x x _ _ _ _ _ _",
-    "_ _ _ _ _ _ _ x _ _ _ _ x _ _ _ _ _ _ _",
-    "_ _ _ _ _ x x x _ _ _ _ x x x _ _ _ _ _",
-  }
-}
+			name = 'LAYOUT_pretty',
+			variant = 'zmk',
+			layout = {
+				'x x x x x x x _ _ _ _ _ _ x x x x x x x',
+				'x x x x x x x _ _ _ _ _ _ x x x x x x x',
+				'x x x x x x x x x _ _ x x x x x x x x x',
+				'x x x x x x _ _ x _ _ x _ _ x x x x x x',
+				'x x x x x _ x x x _ _ x x x _ x x x x x',
+			},
+		}
 
 require("qmk").setup(conf)
