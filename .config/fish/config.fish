9e68e33e53c59919d56affde9343151bab627d59
Richard Steinbrueck <richard.steinbrueck@userlike.com>
2024-02-03 19:58:32 +0100

    💾 save!

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -314,3 +314,16 @@ if test -e $op
 end
 
 direnv hook fish | source
+
+#######################################################################
+#                                tmux                                 #
+#######################################################################
+
+if status is-interactive
+and not set -q TMUX
+    if tmux has-session -t home
+	exec tmux attach-session -t home
+    else
+        tmux new-session -s home
+    end
+end
