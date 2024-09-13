0f9e3cf6ccfa63ab6454aa83b5ad745a029e2344
Richard Steinbrueck <richard.steinbrueck@userlike.com>
2024-09-13 15:20:36 +0200

    fix: remove image.nvim again

--- .config/nvim/lua/packagemanager.lua
+++ .config/nvim/lua/packagemanager.lua
@@ -15,8 +15,4 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("plugins", {
-	rocks = {
-		hererocks = true,
-	},
-})
+require("lazy").setup("plugins")
--- .config/nvim/lua/plugins/render-markdown.lua
+++ .config/nvim/lua/plugins/render-markdown.lua
@@ -1,16 +1,9 @@
 -- Add "3rd/image.nvim" for image support
 return {
-	{
-		"3rd/image.nvim",
-		opts = {},
-		dependencies = {
-			"leafo/magick",
-		},
-	},
 	{ "iamcco/markdown-preview.nvim", build = "cd app && npm install" },
 	{
 		"MeanderingProgrammer/render-markdown.nvim",
 		opts = {},
-		dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons", "3rd/image.nvim" },
+		dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
 	},
 }
