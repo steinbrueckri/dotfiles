cde02c59ec1d95f99c33cd677d2d361995b83236
Richard Steinbrueck <richard.steinbrueck@userlike.com>
2024-10-30 15:11:59 +0100

    💾 save!

--- .config/nvim/lua/plugins/render-markdown.lua
+++ .config/nvim/lua/plugins/render-markdown.lua
@@ -1,14 +1,18 @@
 -- Add "3rd/image.nvim" for image support
 return {
-	{
-		"iamcco/markdown-preview.nvim",
-		event = "VeryLazy",
-		build = "cd app && npm install",
-	},
-	{
-		"MeanderingProgrammer/render-markdown.nvim",
-		event = "VeryLazy",
-		opts = {},
-		dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
-	},
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    build = "cd app && yarn install",
+    init = function()
+      vim.g.mkdp_filetypes = { "markdown" }
+    end,
+    ft = { "markdown" },
+  },
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    event = "VeryLazy",
+    opts = {},
+    dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
+  },
 }
